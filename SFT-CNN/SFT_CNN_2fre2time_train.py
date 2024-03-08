b053004312715ca6082106333f1ebe8fc910c6cf
Tim Lee <Tim8_Lee@pegatroncorp.com>
2024-03-08 18:35:40 +0800

    message

--- SFT-CNN/SFT_CNN_2fre2time_train.py
+++ SFT-CNN/SFT_CNN_2fre2time_train.py
@@ -1,18 +1,38 @@
-from keras.layers import Input, Dense, Dropout, Convolution2D, MaxPool2D, normalization
+from keras.layers import Input, Dense, Dropout, Convolution2D, MaxPool2D, BatchNormalization
 from keras.models import Model, Sequential
 from keras.optimizers import SGD, Adam, RMSprop
 from keras.callbacks import ModelCheckpoint
 from keras.models import load_model
+from tensorflow.keras.layers import MultiHeadAttention, LayerNormalization
 from numpy import *
 import numpy as np
 import numpy.linalg as LA
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+import sys
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import tensorflow as tf
-config = tf.ConfigProto()
+config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth=True   #allow growth
-session = tf.Session(config=config)
 import scipy.io as sio
+
+from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+from tf_encodings import TFPositionalEncoding1D
+from tensorflow.keras.layers import Add
+from sparse_attention import SelfAttention, Multi_Head_Attention
+from tensorflow.keras.layers import Conv2D
+
+epochs_num = 1
+batch_size_num = 32
+encoder_block_num = 3
+decoder_block_num = 3
+learning_rate_num = 1e-4
+key_dim_num = 256
+print("TensorFlow 版本:", tf.__version__)
+print("epochs_num = ", epochs_num)
+print("batch_size_num = ", batch_size_num)
+print("encoder_block_num = ", encoder_block_num)
+print("decoder_block_num = ", decoder_block_num)
+print("learning_rate_num = ", learning_rate_num)
 
 Nt=32
 Nt_beam=32
@@ -48,13 +68,13 @@ data_num_train=1000
 data_num_file=1000
 H_train=zeros((data_num_train,Nr,Nt,2*fre), dtype=float)
 H_train_noisy=zeros((data_num_train,Nr,Nt,2*fre*time_steps), dtype=float)
-filedir = os.listdir('D:\\2fre2time_data')
+current_directory = os.getcwd()
+filedir = os.path.join(current_directory, '2fre2time_data')  # type the path of training data
 n=0
 SNRr=0
 SNR_factor=5.9  # compensate channel power gain to approximate to 1
-for filename in filedir:
-    newname = os.path.join('D:\\2fre2time_data', filename)
-    #print(newname)
+for filename in os.listdir(filedir):
+    newname = os.path.join(filedir, filename)
     data = sio.loadmat(newname)
     channel = data['ChannelData_fre']    
     for i in range(data_num_file):
@@ -85,19 +105,20 @@ row_num=np.unique(index1[0])
 H_train=np.delete(H_train,row_num,axis=0)
 H_train_noisy=np.delete(H_train_noisy,row_num,axis=0)
 print(len(row_num))
-print(H_train.shape,H_train_noisy.shape)
+print("H_train shape = ", H_train.shape, "H_train_noisy shape =", H_train_noisy.shape)
 
 ############## testing set generation ##################
 data_num_test=1000
 data_num_file=1000
 H_test=zeros((data_num_test,Nr,Nt,2*fre), dtype=float)
 H_test_noisy=zeros((data_num_test,Nr,Nt,2*fre*time_steps), dtype=float)
-filedir = os.listdir('D:\\2fre2time_data') # type the path of testing data (Testing data should be different from training data. Here use the same data just for ease of demonstration.)
+current_directory = os.getcwd()
+filedir = os.path.join(current_directory, '2fre2time_data') # type the path of testing data (Testing data should be different from training data. Here use the same data just for ease of demonstration.)
 n=0
 SNRr=0
 SNR_factor=5.9
-for filename in filedir:
-    newname = os.path.join('D:\\2fre2time_data', filename)
+for filename in os.listdir(filedir):
+    newname = os.path.join(filedir, filename)
     data = sio.loadmat(newname)
     channel = data['ChannelData_fre']
     for i in range(data_num_file):
@@ -133,44 +154,131 @@ print(((H_test)**2).mean())
 
 K=3
 input_dim=(Nr,Nt,2*fre*time_steps)
-model = Sequential()
-model.add(Convolution2D(filters=64, kernel_size=(K,K), padding='Same', activation='relu', input_shape=input_dim))
-model.add(normalization.BatchNormalization())
-model.add(Convolution2D(filters=64, kernel_size=(K,K), padding='Same', activation='relu'))
-model.add(normalization.BatchNormalization())
-model.add(Convolution2D(filters=64, kernel_size=(K,K), padding='Same', activation='relu'))
-model.add(normalization.BatchNormalization())
-model.add(Convolution2D(filters=64, kernel_size=(K,K), padding='Same', activation='relu'))
-model.add(normalization.BatchNormalization())
-model.add(Convolution2D(filters=64, kernel_size=(K,K), padding='Same', activation='relu'))
-model.add(normalization.BatchNormalization())
-model.add(Convolution2D(filters=64, kernel_size=(K,K), padding='Same', activation='relu'))
-model.add(normalization.BatchNormalization())
-model.add(Convolution2D(filters=64, kernel_size=(K,K), padding='Same', activation='relu'))
-model.add(normalization.BatchNormalization())
-model.add(Convolution2D(filters=64, kernel_size=(K,K), padding='Same', activation='relu'))
-model.add(normalization.BatchNormalization())
-model.add(Convolution2D(filters=64, kernel_size=(K,K), padding='Same', activation='relu'))
-model.add(normalization.BatchNormalization())
-model.add(Convolution2D(filters=2*fre, kernel_size=(K,K), padding='Same', activation='tanh'))
+reshape_input_dim = (1, int(4096 / key_dim_num), key_dim_num)
+num_heads = 4  # Number of attention heads
+dropout_rate = 0.1
+
+# Define the input layer
+# change here
+#inputs = Input(shape=input_dim)
+#key_dim_num = 4
+inputs = Input(shape=reshape_input_dim)
+reshape_type = (0, 1, 2, 3)
+
+H_train = np.tile(H_train, (1, 1, 2))
+H_test = np.tile(H_test, (1, 1, 2))
+#print("after H_train shape = ", H_train.shape)
+#print("after H_test shape = ", H_test.shape)
+
+# transpose
+H_train_noisy = np.transpose(H_train_noisy, reshape_type)
+H_train = np.transpose(H_train, reshape_type)
+H_test_noisy = np.transpose(H_test_noisy, reshape_type)
+H_test = np.transpose(H_test, reshape_type)
+# change here
+# 将 H_train_noisy, H_train, H_test_noisy, H_test 调整为形状为 (None, 1, int(2048 / key_dim_num), key_dim_num) 的数组
+H_train_noisy = np.reshape(H_train_noisy, (-1, 1, int(4096 / key_dim_num), key_dim_num))
+H_train = np.reshape(H_train, (-1, 1, int(4096 / key_dim_num), key_dim_num))
+H_test_noisy = np.reshape(H_test_noisy, (-1, 1, int(4096 / key_dim_num), key_dim_num))
+H_test = np.reshape(H_test, (-1, 1, int(4096 / key_dim_num), key_dim_num))
+
+# Add a rescaling layer to normalize inputs
+x = Rescaling(scale=1.0 / scale)(inputs)
+
+# learn positional encoding
+# position_encoding = Conv2D(filters=key_dim_num, kernel_size=(K, K), padding='Same', activation='relu')(x)
+# Returns the position encoding only
+positional_encoding_model = TFPositionalEncoding1D(256)
+# change here
+position_encoding = positional_encoding_model(tf.zeros((1,int(4096 / key_dim_num),key_dim_num)))
+position_encoding = tf.expand_dims(position_encoding, axis=0)
+position_encoding = tf.tile(position_encoding, multiples=[999, 1, 1, 1])
+H_train_noisy = Add()([H_train_noisy, position_encoding])
+H_train = Add()([H_train, position_encoding])
+H_test_noisy = Add()([H_test_noisy, position_encoding])
+H_test = Add()([H_test, position_encoding])
+
+enc_output = None
+# Transformer Encoder Layer
+for _ in range(encoder_block_num):  # Repeat the encoder encoder_block_num times
+    # Multi-Head Attention
+    # attn_output = MultiHeadAttention(num_heads=num_heads, key_dim=32, dropout=dropout_rate)(x, x)
+    #self_attention_layer = SelfAttention(d_k=256, d_v=256, d_model=256)
+    self_attention_layer = Multi_Head_Attention(d_k=256, d_v=256, d_model=256, num_heads = 4)
+    attn_output = self_attention_layer(x)
+    # Add & Norm
+    x = Add()([x, attn_output])
+    x = LayerNormalization(epsilon=1e-6)(x)
+
+    # Feed Forward Layer
+    ff_output = Dense(units=key_dim_num, activation='relu')(x)
+    x = Add()([x, ff_output])
+    x = LayerNormalization(epsilon=1e-6)(x)
+
+enc_output = x
+
+# Transformer Decoder Layer
+for _ in range(decoder_block_num):  # Repeat the decoder decoder_block_num times
+    # change here
+    # sequence mask
+    mask = tf.sequence_mask([16], maxlen=16, dtype=tf.float32)
+    #mask = tf.sequence_mask([32], maxlen=32, dtype=tf.float32)
+    mask = tf.expand_dims(tf.expand_dims(mask, axis=0), axis=0)
+
+    # Masked Multi-Head Attention (self-attention on decoder inputs)
+    # attn_output = MultiHeadAttention(num_heads=num_heads, key_dim=32, dropout=dropout_rate)(x, x, attention_mask=mask)
+    # 创建 SelfAttention 层实例
+    masked_self_attention_layer = Multi_Head_Attention(d_k=256, d_v=256, d_model=256, num_heads = 4)
+    attn_output = masked_self_attention_layer(x, mask = mask)
+
+    # Add & Norm
+    x = Add()([x, attn_output])
+    x = LayerNormalization(epsilon=1e-6)(x)
+
+    # Multi-Head Attention (attention to encoder outputs)
+    enc_output = enc_output  # Assuming encoder output is available
+    # attn_output = MultiHeadAttention(num_heads=num_heads, key_dim=32, dropout=dropout_rate)(x, enc_output)
+    cross_attention_layer = Multi_Head_Attention(d_k=256, d_v=256, d_model=256, num_heads = 4)
+    attn_output = cross_attention_layer(x, enc_output = enc_output)
+    # Add & Norm
+    x = Add()([x, attn_output])
+    x = LayerNormalization(epsilon=1e-6)(x)
+
+    # Feed Forward Layer
+    ff_output = Dense(units=key_dim_num, activation='relu')(x)
+    x = Add()([x, ff_output])
+    x = LayerNormalization(epsilon=1e-6)(x)
+
+
+# Output layer
+outputs = Conv2D(filters=key_dim_num, kernel_size=(K, K), padding='Same', activation='tanh')(x)
+
+# Create the model
+model = Model(inputs=inputs, outputs=outputs)
+
+# Compile the model
+adam=Adam(learning_rate=learning_rate_num, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+model.compile(optimizer='adam', loss='mse')
+
+# Print model summary
+model.summary()
+
 
 # checkpoint
-filepath='CNN_UMi_3path_2fre2time_SNR10dB_200ep.hdf5'
+filepath='CNN_UMi_3path_2fre_SNRminus10dB_200ep.tf'
 
 checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 
-adam=Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-model.compile(optimizer=adam, loss='mse')
-model.fit(H_train_noisy, H_train, epochs=200, batch_size=128, callbacks=callbacks_list, verbose=2, shuffle=True, validation_split=0.1)
-
+print("H_train shape = ", H_train.shape, "H_train_noisy shape =", H_train_noisy.shape)
+model.fit(H_train_noisy, H_train, epochs=epochs_num, batch_size=batch_size_num, callbacks=callbacks_list, verbose=2, shuffle=True, validation_split=0.1)
 # load model
-CNN = load_model('CNN_UMi_3path_2fre2time_SNR10dB_200ep.hdf5')
+CNN = tf.keras.models.load_model('CNN_UMi_3path_2fre_SNRminus10dB_200ep.tf')
 
 decoded_channel = CNN.predict(H_test_noisy)
 nmse2=zeros((data_num_test-len(row_num),1), dtype=float)
 for n in range(data_num_test-len(row_num)):
-    MSE=((H_test[n,:,:,:]-decoded_channel[n,:,:,:])**2).sum()
-    norm_real=((H_test[n,:,:,:])**2).sum()
+    MSE = tf.reduce_sum(tf.square(H_test - decoded_channel))
+    norm_real = tf.reduce_sum(tf.square(H_test))
     nmse2[n]=MSE/norm_real
-print(nmse2.sum()/(data_num_test-len(row_num)))  # calculate NMSE of current training stage
+print("NMSE = ", nmse2.sum()/(data_num_test-len(row_num)))  # calculate NMSE of current training stage
---
+++ SFT-CNN/sparse_attention.py
@@ -0,0 +1,169 @@
+import tensorflow as tf
+import numpy as np
+
+class SelfAttention(tf.keras.layers.Layer):
+    def __init__(self, d_k, d_v, d_model, **kwargs):
+        super(SelfAttention, self).__init__(**kwargs)
+        self.d_k = d_k  # Dimension of key vector
+        self.d_v = d_v  # Dimension of value vector
+        self.d_model = d_model  # Dimension of model (same as input feature dimension)
+
+        # Create trainable weights with appropriate shapes
+        self.Wq = self.add_weight(name='Wq', shape=(self.d_model, d_k), initializer='glorot_uniform', trainable=True)
+        self.Wk = self.add_weight(name='Wk', shape=(self.d_model, d_k), initializer='glorot_uniform', trainable=True)
+        self.Wv = self.add_weight(name='Wv', shape=(self.d_model, d_v), initializer='glorot_uniform', trainable=True)
+
+    def call(self, inputs, mask=None):
+        # Reshape input to facilitate efficient matrix multiplication
+        batch_size, seq_len, feature_dim = inputs[0], 8, 256
+        inputs_reshaped = tf.squeeze(inputs, axis=1)  # (B, L, F)
+
+        # Project input features to query, key, and value vectors
+        Q = tf.matmul(inputs_reshaped, self.Wq)  # (B, L, d_k)
+        K = tf.matmul(inputs_reshaped, self.Wk)  # (B, L, d_k)
+        V = tf.matmul(inputs_reshaped, self.Wv)  # (B, L, d_v)
+
+        # Scaled dot-product attention
+        scores = tf.matmul(Q, K, transpose_b=True) / tf.math.sqrt(tf.cast(self.d_k, tf.float32))  # (B, L, L)
+
+        # Apply masking if provided
+        if mask is not None:
+            scores = scores * mask - tf.constant(1e10, dtype=tf.float32) * (1 - mask)
+
+        # Apply softmax for attention weights
+        attention_weights = tf.nn.softmax(scores, axis=-1)  # (B, L, L)
+
+        # Context vector (weighted sum of values)
+        output = tf.matmul(attention_weights, V)  # (B, L, d_v)
+
+        # Reshape output to match original input
+        output = tf.reshape(output, (tf.shape(inputs)[0], 1, 8, 256))
+
+
+        return output
+
+class Multi_Head_Attention(tf.keras.layers.Layer):
+    def __init__(self, d_k, d_v, d_model, num_heads, **kwargs):
+        super(Multi_Head_Attention, self).__init__(**kwargs)
+        self.num_heads = num_heads
+        self.d_model = d_model
+
+        assert d_model % num_heads == 0
+
+        self.d_k = d_k  # Dimension of key vector
+        self.d_v = d_v  # Dimension of value vector
+
+        # Create query, key, and value projections for each head
+        self.Wq = self.add_weight(
+            name='Wq',
+            shape=(num_heads, d_model, d_k),
+            initializer='glorot_uniform',
+            trainable=True
+        )
+        self.Wk = self.add_weight(
+            name='Wk',
+            shape=(num_heads, d_model, d_k),
+            initializer='glorot_uniform',
+            trainable=True
+        )
+        self.Wv = self.add_weight(
+            name='Wv',
+            shape=(num_heads, d_model, d_k),
+            initializer='glorot_uniform',
+            trainable=True
+        )
+
+    def call(self, inputs, mask = None, enc_output = None):
+        #print("call!!!")
+        batch_size =  tf.shape(inputs)[0]
+        shape = inputs.get_shape().as_list()
+        seq_len = int(shape[1] * shape[2] * shape[3] / self.d_model)
+        feature_dim = self.d_model
+        inputs_reshaped = tf.squeeze(inputs, axis=1)  # (B, L, F)
+        if enc_output is not None:
+            enc_output_reshaped = tf.squeeze(enc_output, axis=1)  # (B, L, F)
+
+        total_output = tf.zeros_like(inputs)  # Initialize the total_output
+
+        # Process each head iteratively
+        for i in range(self.num_heads):
+            # Compute the query, key, and value for the i-th head
+            Q = tf.matmul(inputs_reshaped, self.Wq[i])  # (B, L, d_k)
+            if enc_output is not None:
+                K = tf.matmul(enc_output_reshaped, self.Wk[i])  # (B, L, d_k)
+                V = tf.matmul(enc_output_reshaped, self.Wv[i])  # (B, L, d_v)
+            else:
+                K = tf.matmul(inputs_reshaped, self.Wk[i])  # (B, L, d_k)
+                V = tf.matmul(inputs_reshaped, self.Wv[i])  # (B, L, d_v)
+            
+            # Scaled dot-product attention
+            scores = tf.matmul(Q, K, transpose_b=True) / tf.math.sqrt(tf.cast(self.d_k, tf.float32))  # (B, L, L)
+            
+            # Apply masking if provided
+            if mask is not None:
+                scores = scores * mask - tf.constant(1e10, dtype=tf.float32) * (1 - mask)
+            
+            # Apply sparse attention mask
+            mask = atrous_self_attention_mask(N = seq_len, dilation_rate = 2)
+            # mask = local_self_attention_mask(N = seq_len, window_size = 2)
+            # mask = stride_sparse_self_attention_mask(N = seq_len, local_range = 2, stride = 2)
+            scores = scores * mask - tf.constant(1e10, dtype=tf.float32) * (1 - mask)
+
+            # Apply softmax for attention weights
+            attention_weights = tf.nn.softmax(scores, axis=-1)  # (B, L, L)
+            
+            # Context vector (weighted sum of values)
+            output = tf.matmul(attention_weights, V)  # (B, L, d_v)
+
+            # Reshape output to match original input
+            output = tf.reshape(output, (tf.shape(inputs)[0], 1, seq_len, feature_dim))
+
+            # Sum up the outputs of all heads
+            total_output += output
+        
+        # Calculate the average
+        total_output = total_output / self.num_heads
+        #print("inputs shape = ", inputs) # (None, 1, 8, 256)
+        #print("inputs_reshaped shape = ", inputs_reshaped) # (None, 8, 256)
+        #print("Q shape = ", Q) # (None, 8, 256)
+        #print("scores shape = ", scores) # (None, 8, 8)
+        #print("attention_weights shape = ", attention_weights) # (None, 8, 8)
+        #print("total_output shape = ", total_output) # (None, 1, 8, 256)
+
+        return total_output
+
+def atrous_self_attention_mask(N, dilation_rate):
+    # [[1. 0. 1. 0. 1. 0.]
+    #  [0. 1. 0. 1. 0. 1.]
+    #  [1. 0. 1. 0. 1. 0.]
+    #  [0. 1. 0. 1. 0. 1.]
+    #  [1. 0. 1. 0. 1. 0.]
+    #  [0. 1. 0. 1. 0. 1.]]
+    mask = np.zeros((N, N))
+    for i in range(N):
+        for j in range(N):
+            if abs(i - j) % dilation_rate == 0:
+                mask[i, j] = 1
+    return mask
+
+def local_self_attention_mask(N, window_size):
+    # [[1. 1. 1. 0. 0. 0.]
+    #  [1. 1. 1. 1. 0. 0.]
+    #  [1. 1. 1. 1. 1. 0.]
+    #  [0. 1. 1. 1. 1. 1.]
+    #  [0. 0. 1. 1. 1. 1.]
+    #  [0. 0. 0. 1. 1. 1.]]
+    mask = np.zeros((N, N))
+    for i in range(N):
+        for j in range(N):
+            if abs(i - j) <= window_size:
+                mask[i, j] = 1
+    return mask
+
+def stride_sparse_self_attention_mask(N, local_range, stride):
+    mask = np.zeros((N, N))
+    for i in range(N):
+        for j in range(N):
+            if abs(j - i) <= local_range or abs(j - i) % stride == 0:
+                mask[i, j] = 1
+    return mask
