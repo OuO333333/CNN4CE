f449f9638761cb16a07bd1a4f01adada983d4b8e
Tim Lee <Tim8_Lee@pegatroncorp.com>
2024-05-06 17:54:50 +0800

    message

--- SPR-CNN/loop.py
+++ SPR-CNN/loop.py
@@ -14,15 +14,15 @@ with open('output.txt', 'a') as f:
     # 恢复原始的标准输出
     sys.stdout = original_stdout
 # 要执行的命令的基础部分
-base_command = "python3 modality-aware.py"
+base_command = "python3 main.py"
 
 # 要执行的 SNR_dB 参数的范围
-SNR_dB_range = range(-10, 101, 5)
+SNR_dB_range = range(-10, 21, 5)
 
 # 遍历 SNR_dB 参数的范围
 for SNR_dB in SNR_dB_range:
     # 构建完整的命令字符串
-    command = f"{base_command} 0"
+    command = f"{base_command} -10"
     
     # 执行命令
     result = subprocess.run(command, shell=True)
